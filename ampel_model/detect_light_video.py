386d7c930d60511040fb5899df2b84965245fa8e
Corbey96 <59054305+Corbey96@users.noreply.github.com>
2020-02-02 00:13:35 +0000

    Add files via upload

--- ampel_model/detect_light_video.py
+++ ampel_model/detect_light_video.py
@@ -1,20 +1,22 @@
 import cv2
 import numpy as np
 
-trafficlight_green_cascade = cv2.CascadeClassifier("traffic_light.xml")
-trafficlight_red_cascade = cv2.CascadeClassifier("traffic_light.xml")
+trafficlight_green_cascade = cv2.CascadeClassifier("trafficlight_green_cascade.xml")
+trafficlight_red_cascade = cv2.CascadeClassifier("trafficlight_red_cascade.xml")
 
 cap = cv2.VideoCapture(0) #Camera on
 while True:
     img = cap.read()
+    #img = cv2.imread('test.jpg', 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #take image into grayscale
-    green = trafficlight_green_cascade_cascade.detectMultiScale(gray, 1.3, 5)
-    red = trafficlight_red_cascade_cascade.detectMultiScale(gray, 1.3, 5)
+    green = trafficlight_green_cascade.detectMultiScale(gray, 1.3, 5)
+    red = trafficlight_red_cascade.detectMultiScale(gray, 1.3, 5)
     for(x,y,w,h) in green: #draw a rectangle around the detected stop
         cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2) #color green, line width = 2
         font = cv2.FONT_HERSHEY_SIMPLEX #define font for text output
         cv2.putText(img, 'green light', (x-5, y-5), font, 1, (0, 255, 0), 3, cv2.LINE_AA) #ampel einblenden
-        
+       # print("detectet")
+ 
     for(x,y,w,h) in red: #draw a rectangle around the detected stop
         cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2) #color green, line width = 2
         font = cv2.FONT_HERSHEY_SIMPLEX #define font for text output
